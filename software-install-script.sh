f38bc068df53e7f45d66ce00f7defea444ba6a4d
Keijo Pitkänen <124756718+KeijoPitkanen@users.noreply.github.com>
2024-11-16 01:32:40 +0200

    Add files via upload

---
+++ software-install-script.sh
@@ -0,0 +1,13 @@
+echo Updating Pacman
+sudo pacman -Syu
+echo Installing yay
+sudo pacman -S --needed base-devel git
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+yay --version
+echo Yay install successful
+echo installing software from pacman
+sudo pacman -S steam thunderbird vlc telegram-desktop bleachbit kitty neovim gedit jellyfin-server jellyfin-web krita obs-studio qbittorrent speedcrunch
+yay -S vscodium mission-center popsicle whatsie
+echo Software installed

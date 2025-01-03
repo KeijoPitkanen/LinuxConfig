add1496304fec03ac718dddb9d811e7d1cf42be7
Keijo Pitkänen <124756718+KeijoPitkanen@users.noreply.github.com>
2025-01-03 16:35:46 +0200

    Update software-install-script.sh

--- software-install-script.sh
+++ software-install-script.sh
@@ -7,7 +7,7 @@ cd yay
 makepkg -si
 yay --version
 echo Yay install successful
-echo installing software from pacman
+echo installing tools from pacman
 sudo pacman -S steam thunderbird vlc telegram-desktop bleachbit kitty neovim gedit jellyfin-server jellyfin-web krita obs-studio qbittorrent speedcrunch 
 yay -S vscodium mission-center popsicle whatsie waterfox-bin
 echo Software installed

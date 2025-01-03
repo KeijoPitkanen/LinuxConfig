db39e9a4dbf7732b6a7336fcd936e75ad666a938
Keijo Pitkänen <124756718+KeijoPitkanen@users.noreply.github.com>
2025-01-03 16:23:15 +0200

    Update software-install-script.sh
    added install for waterfox

--- software-install-script.sh
+++ software-install-script.sh
@@ -8,6 +8,6 @@ makepkg -si
 yay --version
 echo Yay install successful
 echo installing software from pacman
-sudo pacman -S steam thunderbird vlc telegram-desktop bleachbit kitty neovim gedit jellyfin-server jellyfin-web krita obs-studio qbittorrent speedcrunch
-yay -S vscodium mission-center popsicle whatsie
+sudo pacman -S steam thunderbird vlc telegram-desktop bleachbit kitty neovim gedit jellyfin-server jellyfin-web krita obs-studio qbittorrent speedcrunch 
+yay -S vscodium mission-center popsicle whatsie waterfox-bin
 echo Software installed

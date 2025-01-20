584ff0f11e08f1203bcab1613ac1cc56b86c0a4f
Keijo Pitkänen <124756718+KeijoPitkanen@users.noreply.github.com>
2025-01-20 19:19:43 +0200

    Update software-install-script.sh

--- software-install-script.sh
+++ software-install-script.sh
@@ -8,6 +8,6 @@ makepkg -si
 yay --version
 echo Yay install successful
 echo installing tools from pacman
-sudo pacman -S steam thunderbird vlc telegram-desktop bleachbit kitty neovim gedit jellyfin-server jellyfin-web krita obs-studio qbittorrent speedcrunch 
+sudo pacman -S steam fzf thunderbird vlc telegram-desktop bleachbit kitty neovim gedit jellyfin-server jellyfin-web krita obs-studio qbittorrent speedcrunch 
 yay -S vscodium mission-center popsicle whatsie waterfox-bin
 echo Software installed

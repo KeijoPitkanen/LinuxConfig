f73f9d8deece0b2e774418c0fd470de1d1765c59
skula <mikko.k.pitkanen02@gmail.com>
2025-06-18 09:29:22 +0300

    added mission-center to popOs software-install-script

--- software-install-script-popOs.sh
+++ software-install-script-popOs.sh
@@ -6,7 +6,7 @@ declare -A packages=(
   [gnome]="desktop environment"
   [kitty]="terminal emulator"
   [krita]="digital painting software"
-  [obs-studio]="streaming and recording software"
+  [obs - studio]="streaming and recording software"
   [qbittorrent]="torrent client"
   [ranger]="terminal file browser"
   [speedcrunch]="calculator"
@@ -21,38 +21,37 @@ declare -A flatpaks=(
   [spotify]="web music player"
   [telegram]="messaging app"
   [waterfox]="web browser"
-
+  [missioncenter]="system monitor"
 )
 
 echo Updating Packages
 sudo apt update
 sudo apt upgrade
 
-
 read -p "Do you want to install Fastfetch from the PPA? (y/n): " choice
 if [[ "$choice" == "y" || "$choice" == "Y" ]]; then
-    echo "Adding Fastfetch PPA and installing..."
-    sudo add-apt-repository ppa:zhangsongcui3371/fastfetch
-    sudo apt update
-    sudo apt install -y fastfetch
-    echo "Fastfetch installation complete!"
+  echo "Adding Fastfetch PPA and installing..."
+  sudo add-apt-repository ppa:zhangsongcui3371/fastfetch
+  sudo apt update
+  sudo apt install -y fastfetch
+  echo "Fastfetch installation complete!"
 else
-    echo "Fastfetch installation skipped."
+  echo "Fastfetch installation skipped."
 fi
 
 read -p "Do you want to install Neovim from source? (y/n): " choice
 if [[ "$choice" == "y" || "$choice" == "Y" ]]; then
-    echo "Building Neovim from source..."
-    sudo apt-get update
-    sudo apt-get install -y ninja-build gettext cmake curl build-essential
-    git clone https://github.com/neovim/neovim
-    cd neovim || exit
-    git checkout stable
-    make CMAKE_BUILD_TYPE=RelWithDebInfo
-    sudo make install
-    echo "Neovim installation complete!"
+  echo "Building Neovim from source..."
+  sudo apt-get update
+  sudo apt-get install -y ninja-build gettext cmake curl build-essential
+  git clone https://github.com/neovim/neovim
+  cd neovim || exit
+  git checkout stable
+  make CMAKE_BUILD_TYPE=RelWithDebInfo
+  sudo make install
+  echo "Neovim installation complete!"
 else
-    echo "Neovim installation skipped."
+  echo "Neovim installation skipped."
 fi
 
 echo "Installing packages"
